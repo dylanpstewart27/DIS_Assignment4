fa8c656ad710e28038a9abcc47283bb9710d0aa6
Dylan <Dylan@LAPTOP-HU97QSV9>
2021-04-12 14:43:26 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DIS_Assignment4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
