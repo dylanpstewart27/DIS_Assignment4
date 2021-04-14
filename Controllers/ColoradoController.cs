ae87bed78b7b98cd4920f2125add525caffbaded
Dylan <Dylan@LAPTOP-HU97QSV9>
2021-04-14 12:32:10 -0400

    Added Controllers for some of the States, and displayed some of the crime stats for those states. Still needed is to add the full list of crimes, add more states, and have CRUD fucntionality for each states records

---
+++ Controllers/ColoradoController.cs
@@ -0,0 +1,62 @@
+﻿using DIS_Assignment4.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+using System.Net.Http;
+
+namespace DIS_Assignment4.Controllers
+{
+    public class ColoradoController : Controller
+    { 
+
+         HttpClient httpClient;
+
+        static string BASE_URL = "https://api.usa.gov/crime/fbi/sapi/";
+    static string API_KEY = "lseCYNwUaETkleRct4GGR4iKNbwzpJQ5rTUNPmkK"; //Add your API key here inside ""
+    
+        public IActionResult ColoradoStats()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            string CRIME_API_PATH = BASE_URL + "api/data/arrest/states/offense/CO/all/2016/2019?API_KEY=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv";
+            string CrimeData = "";
+
+            ColoradoRootobject results = null;
+
+            httpClient.BaseAddress = new Uri(CRIME_API_PATH);
+
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(CRIME_API_PATH).GetAwaiter().GetResult();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    CrimeData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!CrimeData.Equals(""))
+                {
+
+                    results = JsonConvert.DeserializeObject<ColoradoRootobject>(CrimeData);
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+            return View(results);
+
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,24 +5,32 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+using System.Net.Http;
 
 namespace DIS_Assignment4.Controllers
 {
     public class HomeController : Controller
     {
+        HttpClient httpClient;
+
+        static string BASE_URL = "https://api.usa.gov/crime/fbi/sapi/";
+        static string API_KEY = "lseCYNwUaETkleRct4GGR4iKNbwzpJQ5rTUNPmkK"; //Add your API key here inside ""
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult AboutUs()
         {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
-        public IActionResult Contact()
+        public IActionResult States()
         {
             ViewData["Message"] = "Your contact page.";
 
@@ -39,5 +47,8 @@ namespace DIS_Assignment4.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        
+        
     }
 }
