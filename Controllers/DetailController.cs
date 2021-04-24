7a4159789c7103f5c224af2c4576d1e3d07e9e15
Olasupo1 <lanreolasupo@yahoo.com>
2021-04-24 00:47:15 -0400

    Create and Update

--- Controllers/DetailController.cs
+++ Controllers/DetailController.cs
@@ -41,9 +41,9 @@ namespace DIS_Assignment4.Controllers
 
         public IActionResult Details(string key)
         {
-            
+            ViewData["key"] = key;
             List<Datum> list = new List<Datum>();
-           
+
 
             //List<SectorSourceAnnual> list = new List<SectorSourceAnnual>();
             //var sectorSourceData = _context.AnnualEnergyConsumption.Where(t => t.sector.SectorName == sector & t.energysource.SourceName == source).OrderByDescending(y => y.Year);
@@ -58,13 +58,51 @@ namespace DIS_Assignment4.Controllers
             //return View(details);
 
             //var query = dbContext.Datums.Where(d => d.key == key);
-            foreach(Datum x in dbContext.Datums.Where(d => d.key == key).OrderByDescending(d => d.data_year))
+
+            foreach (Datum x in dbContext.Datums.Where(d => d.key == key).OrderByDescending(d => d.data_year))
             {
                 list.Add(x);
             }
-            
+
 
             return View(list);
+        }
+
+        [HttpGet("/Detail/Create/{key}")]
+        public IActionResult Create([FromRoute] string key)
+        {
+            ViewData["key"] = key;
+            return View();
+        }
+
+        public IActionResult CreateDatum(string key, int data_year, int month_num, int value)
+        {
+            var datum = new Datum
+            {
+                data_year = data_year,
+                key = key,
+                month_num = month_num,
+                value = value
+            };
+            dbContext.Datums.Add(datum);
+            dbContext.SaveChanges();
+            return RedirectPreserveMethod("Detail/Details");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return View(dbContext.Datums.Where(d => d.ID == id));
+        }
+
+        public IActionResult Update(int ID, int value)
+        {
+            var datum = dbContext.Datums.SingleOrDefault(d => d.ID == ID);
+            datum.value = value;
+
+            dbContext.Datums.Update(datum);
+            dbContext.SaveChanges();
+
+            return View("Edit", dbContext.Datums.Where(d => d.ID == ID));
         }
 
         public IActionResult Delete(string key, int data_year, int value)
